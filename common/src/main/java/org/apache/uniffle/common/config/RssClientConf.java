6fee8e58c2ec9b883e300bc6af2ec87650d9638b
Junfan Zhang <zuston@apache.org>
2025-02-08 17:22:51 +0800

    [#291] feat(client): Introduce PrefetchableClientReadHandler to support async read (#2365)
    1. Introduce PrefetchableClientReadHandler to support async read. And this will be disabled by default.
    2. Apply for the memory/localfile/hdfs read handler
    
    Recently I found some important spark jobs are slow due to the lots of shuffle read operations. If we could support async read, the job's performance will be improved.
    
    So this PR is the callback for #291. almost 3 years ago!
    
    Yes. Some configs are introduced
    1. `rss.client.read.prefetch.enabled`
    2. `rss.client.read.prefetch.capacity`
    3. `rss.client.read.prefetch.timeoutSec`
    
    1. Unit tests
    
    ---------
    
    Co-authored-by: Junfan Zhang <zhangjunfan@qiyi.com>

--- common/src/main/java/org/apache/uniffle/common/config/RssClientConf.java
+++ common/src/main/java/org/apache/uniffle/common/config/RssClientConf.java
@@ -228,8 +228,26 @@ public class RssClientConf {
           .withDescription("the extra java properties could be configured by this option");
 
   public static final ConfigOption<Integer> RSS_CLIENT_GRPC_EVENT_LOOP_THREADS =
-          ConfigOptions.key("rss.client.grpc.nettyEventLoopThreads")
-                  .intType()
-                  .defaultValue(-1)
-                  .withDescription("the event loop threads of netty impl for grpc");
+      ConfigOptions.key("rss.client.grpc.nettyEventLoopThreads")
+          .intType()
+          .defaultValue(-1)
+          .withDescription("the event loop threads of netty impl for grpc");
+
+  public static final ConfigOption<Boolean> RSS_CLIENT_PREFETCH_ENABLED =
+      ConfigOptions.key("rss.client.read.prefetch.enabled")
+          .booleanType()
+          .defaultValue(false)
+          .withDescription("Read prefetch switch that will be disabled by default");
+
+  public static final ConfigOption<Integer> RSS_CLIENT_PREFETCH_CAPACITY =
+      ConfigOptions.key("rss.client.read.prefetch.capacity")
+          .intType()
+          .defaultValue(4)
+          .withDescription("Read prefetch capacity");
+
+  public static final ConfigOption<Integer> READ_CLIENT_PREFETCH_TIMEOUT_SEC =
+      ConfigOptions.key("rss.client.read.prefetch.timeoutSec")
+          .intType()
+          .defaultValue(120)
+          .withDescription("Read prefetch timeout seconds");
 }
