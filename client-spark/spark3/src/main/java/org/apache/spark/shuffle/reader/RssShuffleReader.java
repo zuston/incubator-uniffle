bc307a4f05846e5994f222aab35b554f1f2034ae
Junfan Zhang <zuston@apache.org>
2025-08-27 10:15:00 +0800

    [#2592] fix(spark): Ignore failure when reporting shuffle read metrics to driver (#2593)
    ### What changes were proposed in this pull request?
    
    Ignore failure when reporting shuffle read metrics to driver
    
    ### Why are the changes needed?
    
    fix #2592 
    
    ### Does this PR introduce _any_ user-facing change?
    
    No.
    
    ### How was this patch tested?
    
    Needn't

--- client-spark/spark3/src/main/java/org/apache/spark/shuffle/reader/RssShuffleReader.java
+++ client-spark/spark3/src/main/java/org/apache/spark/shuffle/reader/RssShuffleReader.java
@@ -375,30 +375,34 @@ public class RssShuffleReader<K, C> implements ShuffleReader<K, C> {
     if (managerClientSupplier != null) {
       ShuffleManagerClient client = managerClientSupplier.get();
       if (client != null) {
-        RssReportShuffleReadMetricResponse response =
-            client.reportShuffleReadMetric(
-                new RssReportShuffleReadMetricRequest(
-                    context.stageId(),
-                    shuffleId,
-                    context.taskAttemptId(),
-                    shuffleServerReadCostTracker.list().entrySet().stream()
-                        .collect(
-                            Collectors.toMap(
-                                Map.Entry::getKey,
-                                x ->
-                                    new RssReportShuffleReadMetricRequest.TaskShuffleReadMetric(
-                                        x.getValue().getDurationMillis(),
-                                        x.getValue().getReadBytes(),
-                                        x.getValue().getMemoryReadDurationMillis(),
-                                        x.getValue().getMemoryReadBytes(),
-                                        x.getValue().getLocalfileReadDurationMillis(),
-                                        x.getValue().getLocalfileReadBytes(),
-                                        x.getValue().getHadoopReadLocalFileDurationMillis(),
-                                        x.getValue().getHadoopReadLocalFileBytes()))),
-                    isShuffleReadFailed,
-                    shuffleReadReason));
-        if (response != null && response.getStatusCode() != StatusCode.SUCCESS) {
-          LOG.error("Errors on reporting shuffle read metrics to driver");
+        try {
+          RssReportShuffleReadMetricResponse response =
+              client.reportShuffleReadMetric(
+                  new RssReportShuffleReadMetricRequest(
+                      context.stageId(),
+                      shuffleId,
+                      context.taskAttemptId(),
+                      shuffleServerReadCostTracker.list().entrySet().stream()
+                          .collect(
+                              Collectors.toMap(
+                                  Map.Entry::getKey,
+                                  x ->
+                                      new RssReportShuffleReadMetricRequest.TaskShuffleReadMetric(
+                                          x.getValue().getDurationMillis(),
+                                          x.getValue().getReadBytes(),
+                                          x.getValue().getMemoryReadDurationMillis(),
+                                          x.getValue().getMemoryReadBytes(),
+                                          x.getValue().getLocalfileReadDurationMillis(),
+                                          x.getValue().getLocalfileReadBytes(),
+                                          x.getValue().getHadoopReadLocalFileDurationMillis(),
+                                          x.getValue().getHadoopReadLocalFileBytes()))),
+                      isShuffleReadFailed,
+                      shuffleReadReason));
+          if (response != null && response.getStatusCode() != StatusCode.SUCCESS) {
+            LOG.error("Errors on reporting shuffle read metrics to driver");
+          }
+        } catch (Exception e) {
+          LOG.error("Errors on post shuffle read metric to driver", e);
         }
       }
     }
